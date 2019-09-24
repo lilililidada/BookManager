625c0e97c8112d08ecdd0f0deecd7a77a8665734
MR.wu <m13535494368@163.com>
2019-09-24 12:16:57 +0800

    modify 1

--- src/main/java/cn/wzz/BookManager/dao/BookDAO.java
+++ src/main/java/cn/wzz/BookManager/dao/BookDAO.java
@@ -1,7 +1,15 @@
 package cn.wzz.BookManager.dao;
 
-public class BookDAO {
+import org.apache.ibatis.annotations.Insert;
 
+import cn.wzz.BookManager.model.Book;
+
+public interface BookDAO {
+
+	String table_name = "BookManager";
+	String insert_field = "name, author, price";
+	String select_field = "id, status, "+insert_field;
 	
-
+	@Insert({"insert into", table_name, "(", insert_field, ") values (#{name},#{author},#{price})"})
+	int addBook(Book book);
 }
